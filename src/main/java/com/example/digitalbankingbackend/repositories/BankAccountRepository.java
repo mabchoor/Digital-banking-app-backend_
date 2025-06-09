3d6401b1f78c8be5cd1ae779acd39e9a1ffe1f4d
Abderrahmanedev23 <125931479+Abderrahmanedev23@users.noreply.github.com>
2025-06-09 12:12:12 +0100

    Initial commit with clean history

---
+++ src/main/java/com/example/digitalbankingbackend/repositories/BankAccountRepository.java
@@ -0,0 +1,10 @@
+package com.example.digitalbankingbackend.repositories;
+
+import com.example.digitalbankingbackend.entities.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
+    List<BankAccount> findByCustomerId(Long customerId);
+}
